3805c273512cb96ce24355cb3a528f7b8ea7e39e
Illia Lebedko <illya_lebedko@icloud.com>
2024-10-25 20:31:21 +0300

    rebuild code from colinear method to square triangle method

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 a = [0.025, 0.05, 0.075]
 b = [1.64, 1.64, 1.64]
 
-v12 = [a[0]-a[1], b[0]-b[1]]
-v13 = [a[0]-a[2], b[0]-b[2]]
+def squareTriangle():
+    firstBraсket = a[0]*b[1]+a[1]*b[2]+a[2]*b[0]
+    secondBraсket = b[0]*a[1]+b[1]*a[2]+b[2]*a[0]
+    return (firstBraсket-secondBraсket)/2
 
-if v13[0]/v12[0] - v13[1]/v12[1] <= 1e-10:
-    print("colin")
-else:
-    print("no colin")
+a = squareTriangle()
+print(a)
